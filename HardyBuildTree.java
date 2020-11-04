73dbc9e049200a85695844abee61ea1ae29d9d09
Ezekiel Young <eoyoung.2019@smu.edu.sg>
2020-11-04 18:17:56 +0800

    Update HardyBuildTree.java

--- HardyBuildTree.java
+++ HardyBuildTree.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -8,38 +8,46 @@ import jdk.nashorn.internal.parser.TokenStream;
 import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
 
 public class HardyBuildTree {
-    public void split(HashMap<String, Object> node, double maxDepth, int minSize, double depth) {
-        HashMap<String, Object> left = (HashMap<String, Object>) node.get("groups");
-        HashMap<String, Object> right = (HashMap<String, Object>) node.get("groups");
+    public void split(HashMap<String, Object> node, int maxDepth, int minSize, int depth) {
+        ArrayList<ArrayList<Double>> left = ((ArrayList<ArrayList<ArrayList<Double>>>) node.get("groups")).get(0);
+        ArrayList<ArrayList<Double>> right = ((ArrayList<ArrayList<ArrayList<Double>>>) node.get("groups")).get(1);
+
         node.remove("groups");
-        // Check for a no split
-        if (left == null || right == null) {
-            node.replace("right", toTerminal(left.putAll(right)));
-            node.replace("left", node.get("right"));
+
+        // Check for a no split 
+        if (left == null) {
+            node.put("left", toTerminal(right));
+            node.put("right", toTerminal(right));
+
+            return;
+        } else if (right == null) {
+            node.put("right", toTerminal(left));
+            node.put("left", toTerminal(left));
+
             return;
         }
 
         // Check for max depth
         if (depth >= maxDepth) {
-            node.replace("left", toTerminal(left));
-            node.replace("right", toTerminal(right));
+            node.put("left", toTerminal(left));
+            node.put("right", toTerminal(right));
             return;
         }
 
         // Process left child
         if (left.size() <= minSize) { 
-            node.replace("left", toTerminal(left));
+            node.put("left", toTerminal(left));
         } else {
-            node.replace("left", getSplit(left));
-            split((HashMap<String, Object>)node.get("left"), maxDepth, minSize, depth + 1);
+            node.put("left", getSplit(left));
+            split((HashMap<String, Object>) node.get("left"), maxDepth, minSize, depth + 1);
         }   
 
         // Process right child
         if (right.size() <= minSize) {
-            node.replace("right", toTerminal(right));
+            node.put("right", toTerminal(right));
         } else {
-            node.replace("right", getSplit(right));
-            split((HashMap<String, Object>)node.get("right"), maxDepth, minSize, depth + 1);
+            node.put("right", getSplit(right));
+            split((HashMap<String, Object>) node.get("right"), maxDepth, minSize, depth + 1);
         }
 
     }
@@ -50,20 +58,27 @@ public class HardyBuildTree {
         return root;
     }
 
-    public Object toTerminal(HashMap<String, Object> group) {
-        ArrayList<Object> outcomes = new ArrayList<>();
-        for (String row : group.keySet()) {
-            Object object = ((TokenStream) group.get(row)).get();
-            outcomes.add(object);
-        } 
-        
-        int max = 0;
-        for (Object o : outcomes) {
-            if ((int)o > max) {
-                max = (int)o;
+    public Double toTerminal(ArrayList<ArrayList<Double>> group) {
+
+        HashMap<Double, Integer> map = new HashMap<>(); 
+
+        for (ArrayList<Double> row : group) { 
+            Double label = row.get(row.size() - 1);
+            if (map.containsKey(label)) {
+                map.put(label, map.get(label) + 1);
+            } else {
+                map.put(label, 1);
             }
         }
 
-        return max;
+        Double max = 0.0;
+        Double mostFrequentLabel = 0.0;
+        for (Double key: map.keySet()) {
+            if (map.get(key) > max ) {
+                mostFrequentLabel = key;
+            }
+        }
+
+        return mostFrequentLabel;
     }
 }
